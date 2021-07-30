61941bb0e168371c313b9d9b2ad95aae6a922c21
megane42 <h.kzm.gm@gmail.com>
2021-07-31 05:10:33 +0900

    add team model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,4 @@
 class Game < ApplicationRecord
   belongs_to :event
+  has_many :teams, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_30_191801) do
+ActiveRecord::Schema.define(version: 2021_07_30_192232) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,5 +31,13 @@ ActiveRecord::Schema.define(version: 2021_07_30_191801) do
     t.index ["event_id"], name: "index_games_on_event_id"
   end
 
+  create_table "teams", force: :cascade do |t|
+    t.bigint "game_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["game_id"], name: "index_teams_on_game_id"
+  end
+
   add_foreign_key "games", "events"
+  add_foreign_key "teams", "games"
 end
