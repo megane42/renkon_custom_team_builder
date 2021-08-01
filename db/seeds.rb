8723060d0ea3d48e6fd7c5d7d1177f47692740f6
megane42 <h.kzm.gm@gmail.com>
2021-08-01 22:29:56 +0900

    create master data on seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+role_definition_tank    = RoleDefinition.find_or_create_by(name: "tank")
+role_definition_damage  = RoleDefinition.find_or_create_by(name: "damage")
+role_definition_support = RoleDefinition.find_or_create_by(name: "support")
+
+TeamDefinition.find_or_create_by(name: "team_a")
+TeamDefinition.find_or_create_by(name: "team_b")
+
+SheetDefinition.find_or_create_by(name: "tank_a",    role_definition: role_definition_tank)
+SheetDefinition.find_or_create_by(name: "tank_b",    role_definition: role_definition_tank)
+SheetDefinition.find_or_create_by(name: "damage_a",  role_definition: role_definition_damage)
+SheetDefinition.find_or_create_by(name: "damage_b",  role_definition: role_definition_damage)
+SheetDefinition.find_or_create_by(name: "support_a", role_definition: role_definition_support)
+SheetDefinition.find_or_create_by(name: "support_b", role_definition: role_definition_support)
